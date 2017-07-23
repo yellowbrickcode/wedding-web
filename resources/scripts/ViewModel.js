9a4154c7d0099b1fd0f81517adb5a0dc4dc1be7d
Sarah Williams <sarah@yellowbrickcode.co.uk>
2017-07-23 12:02:17 +0100

    Added submit feedback
    After they submit their RSVP I provide them with some basic visual feedback

--- resources/scripts/ViewModel.js
+++ resources/scripts/ViewModel.js
@@ -1,11 +1,14 @@
 var ko = require("knockout");
 var Guest = require("./Guest");
 var Rsvp = require("./Rsvp");
+var $ = require("jquery");
 
 module.exports = function() {
     var self = this;
 
     this.rsvp = new Rsvp();
+
+    this.result = ko.observable("");
 
     this.setRsvpType = (data, event) => {
         self.rsvp.type(event.currentTarget.id);
@@ -22,5 +25,7 @@ module.exports = function() {
     this.sendRsvp = () => {
         var data = ko.toJSON(self.rsvp);
         console.log(data);
+
+        self.result("success");
     }
 };
